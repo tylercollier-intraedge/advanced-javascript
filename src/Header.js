5f63b65ade03d3e1cb0cb85e38cfcbad462b98a2
Tyler Collier <tyler@intraedge.com>
2019-01-31 16:56:10 -0700

    Add shell of site with header and router

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Header.scss'
+import { NavLink } from 'react-router-dom'
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="main">
+        <h1>Advanced JavaScript</h1>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/references">References</NavLink>
+        </nav>
+      </header>
+    )
+  }
+}
+
+export default Header
---
+++ src/References.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class References extends Component {
+  render() {
+    return (
+      <div>
+        <h2>References</h2>
+      </div>
+    )
+  }
+}
+
+export default References
